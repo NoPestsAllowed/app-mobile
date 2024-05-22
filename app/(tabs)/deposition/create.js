1ed03f1e10b02b4a21fdd35433ebb70645e1bc7e
Basile Favretto <bazil.f@gmail.com>
2024-05-22 14:32:31 +0200

    Create deposition autocomplete -wip-

--- app/(tabs)/deposition/create.js
+++ app/(tabs)/deposition/create.js
@@ -1,128 +1,138 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { Image, StyleSheet, TouchableOpacity, Text, View } from "react-native";
 import ParallaxScrollView from "../../../components/ParallaxScrollView";
 import { ThemedText } from "../../../components/ThemedText";
 import { ThemedView } from "../../../components/ThemedView";
 import { ThemedTextInput } from "../../../components/ThemedTextInput";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function CreateDepositionTab({ navigation }) {
-    const [firstName, setFirstName] = useState("");
-    // const [lastName, setLastName] = useState("");
-    // const [email, setEmail] = useState("");
-    // const [password, setPassword] = useState("");
-    // const [birthDate, setBirthDate] = useState("");
     const [depositionName, setDepositionName] = useState("");
     const [localisation, setLocalisation] = useState("");
     const [ownerEmail, setOwnerEmail] = useState("");
+    const [userLocation, setUserLocation] = useState(null);
 
-    // const [modifyNotifications, setModifyNotifications] = useState(false);
-    // const [authorizeNotifications, setAuthorizeNotifications] = useState(false);
+    const initialLocalisation = {
+        latitude: 48.86667,
+        longitude: 2.333333,
+        latitudeDelta: 0.000922,
+        longitudeDelta: 0.000421,
+    };
+    const [mapLocation, setMapLocation] = useState(initialLocalisation);
 
-// const handleLogout = () => {
-//     navigation.navigate('Home');
-// };
+    useEffect(() => {
+        (async () => {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if (status === "granted") {
+                const location = await Location.watchPositionAsync(
+                    {
+                        distanceInterval: 10,
+                    },
+                    (location) => {
+                        console.log(location);
+                        setUserLocation(location);
+                    }
+                );
+            }
+        })();
+    }, []);
 
-// const handleDeleteAccount = () => {
-//     navigation.navigate('Home');
-// };
-
-// const toggleModifyNotifications = () => {
-//     setModifyNotifications(!modifyNotifications);
-// };
-
-// const toggleAuthorizeNotifications = () => {
-//     setAuthorizeNotifications(!authorizeNotifications);
-// };
-
-return (
-    <ParallaxScrollView
-        headerBackgroundColor={{ light: "grey", dark: "#1D3D47" }}
-        headerImage={
-            <View style={styles.headerContainer}>
-                <Image source={require("../../../assets/images/icon.png")} style={styles.noPestsAllowedLogo} />
-                <View style={styles.rightHeader}>
-                    <Text style={styles.welcomeText}>Welcome, {firstName}!</Text>
-                    <Image source={require("../../../assets/images/avatar1.jpg")} style={styles.avatar} />
-                </View>
-            </View>
+    useEffect(() => {
+        if (userLocation) {
+            setMapLocation({
+                latitude: userLocation.coords.latitude,
+                longitude: userLocation.coords.longitude,
+                latitudeDelta: 0.000922,
+                longitudeDelta: 0.000421,
+            });
         }
-    >
-        <ThemedView style={styles.titleContainer}>
-            <ThemedText type="title" style={styles.title}>Créer une déposition</ThemedText>
-        </ThemedView>
+    }, [userLocation]);
 
-        {/* <ThemedView style={styles.containerMap}>
-            <ThemedText style={styles.mapView}>MAP GEOLOCALISEE</ThemedText>
-        </ThemedView> */}
+    const autocompleteLocation = (value) => {
+        console.log("autocomplete");
+        setLocalisation(value);
+    };
 
-        <ThemedTextInput
-            onChangeText={(value) => setDepositionName(value)} 
-            value={depositionName}
-            placeholder="Nom de la déposition"
-            label="Deposition name"
-            style={[styles.profileInfo, styles.input]}
-        />
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{ light: "grey", dark: "#1D3D47" }}
+            headerImage={<MapView region={mapLocation} style={{ flex: 1 }}></MapView>}
+        >
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title" style={styles.title}>
+                    Créer une déposition
+                </ThemedText>
+            </ThemedView>
 
-        <ThemedTextInput 
-            onChangeText={(value) => setLocalisation(value)} 
-            value={localisation}
-            placeholder="Localisation"
-            label="Localisation"
-            style={[styles.profileInfo, styles.input]}
-        />
+            <ThemedTextInput
+                onChangeText={(value) => setDepositionName(value)}
+                value={depositionName}
+                placeholder="Nom de la déposition"
+                label="Deposition name"
+                style={[styles.profileInfo, styles.input]}
+            />
 
-        <ThemedTextInput 
-            onChangeText={(value) => setOwnerEmail(value)} 
-            value={ownerEmail}
-            placeholder="Email du proprietaire"
-            label="Owner email"
-            style={[styles.profileInfo, styles.input]}
-        />
+            <ThemedTextInput
+                onChangeText={(value) => autocompleteLocation(value)}
+                value={localisation}
+                placeholder="Localisation"
+                label="Localisation"
+                style={[styles.profileInfo, styles.input]}
+            />
 
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Open Camera</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Upload images from phone</Text>
-        </TouchableOpacity>
+            <ThemedTextInput
+                onChangeText={(value) => setOwnerEmail(value)}
+                value={ownerEmail}
+                placeholder="Email du proprietaire"
+                label="Owner email"
+                style={[styles.profileInfo, styles.input]}
+            />
 
-        <TouchableOpacity style={styles.buttonSubmit}>
-            <Text style={styles.submitButton}>Submit</Text>
-        </TouchableOpacity>
-    </ParallaxScrollView>
-);
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Open Camera</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Upload images from phone</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonSubmit}>
+                <Text style={styles.submitButton}>Submit</Text>
+            </TouchableOpacity>
+        </ParallaxScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
     headerContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
         padding: 16,
     },
     noPestsAllowedLogo: {
         height: 100,
         width: 100,
-        top:50,
+        top: 50,
     },
     rightHeader: {
-        flexDirection: 'row',
-        alignItems: 'center',
+        flexDirection: "row",
+        alignItems: "center",
     },
     welcomeText: {
         marginRight: 25,
         fontSize: 24,
-        color: '#A53939',
-        top:50,
+        color: "#A53939",
+        top: 50,
         fontWeight: "800",
     },
     avatar: {
         height: 100,
         width: 100,
         borderRadius: 125,
-        top:50,
+        top: 50,
     },
     titleContainer: {
         flexDirection: "row",
@@ -132,41 +142,40 @@ const styles = StyleSheet.create({
     profileInfo: {
         margin: 3,
         fontSize: 18,
-
     },
     title: {
         fontSize: 32,
-        color: '#A53939',
-        shadowColor: '#888',
+        color: "#A53939",
+        shadowColor: "#888",
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 1,
         shadowRadius: 7,
     },
     input: {
         borderWidth: 1,
-        borderColor: '#A53939',
+        borderColor: "#A53939",
         padding: 5,
         borderRadius: 7,
     },
     button: {
-        backgroundColor: '#A53939',
+        backgroundColor: "#A53939",
         padding: 10,
         margin: 5,
         borderRadius: 10,
-        alignItems: 'center',
-        shadowColor: '#888',
+        alignItems: "center",
+        shadowColor: "#888",
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 1,
         shadowRadius: 7,
         elevation: 3,
     },
     buttonSubmit: {
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         padding: 10,
         margin: 5,
         borderRadius: 10,
-        alignItems: 'center',
-        shadowColor: '#888',
+        alignItems: "center",
+        shadowColor: "#888",
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 1,
         shadowRadius: 7,
@@ -178,16 +187,16 @@ const styles = StyleSheet.create({
     },
     notifications: {
         fontSize: 18,
-        fontStyle: 'italic'
+        fontStyle: "italic",
     },
     buttonText: {
-        color: '#f5f5f5',
+        color: "#f5f5f5",
         fontSize: 18,
     },
     notificationContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
         marginVertical: 5,
     },
     // containerMap: {
