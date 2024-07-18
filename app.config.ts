b7494f835f7a0f066dff125a773f5438d95176b3
Basile Favretto <bazil.f@gmail.com>
2024-07-18 12:50:00 +0200

    Added package name to dynamic config for android

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config as ExpoConfig,
     android: {
+        package: "com.anonymous.nopestsallowed",
         config: {
             googleMaps: {
                 apiKey:  process.env.GGMAPS_API_KEY,
